8ba8e0b0764362387baac193e53a949d26abf1d0
AlliNeuman <alli.neuman@gmail.com>
2019-03-04 10:38:46 -0600

    remove tables that were created for writing blog post, that were not needed for this particular app

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_04_162613) do
+ActiveRecord::Schema.define(version: 2019_03_04_163218) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -19,18 +19,12 @@ ActiveRecord::Schema.define(version: 2019_03_04_162613) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
+  create_table "comments", force: :cascade do |t|
+    t.text "description"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.string "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "category_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
